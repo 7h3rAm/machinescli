8068d5d44c301883301a40c8ca1d90edff039b59
Ankur Tyagi (@7h3rAm) <ankurt.20@gmail.com>
2020-11-08 10:50:42 +0530

    📝  ID:10T Error

--- tryhackme.py
+++ tryhackme.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import utils
+from pprint import pprint
 
 
 class TryHackMe:
@@ -21,25 +22,35 @@ class TryHackMe:
     return utils.post_http(url, data, self.headers)
 
     """
-      curl -s "https://tryhackme.com/api/all-completed-rooms/7h3rAm" | jq
-      curl -s "https://tryhackme.com/api/get-badges" | jq
-      curl -s "https://tryhackme.com/api/get-votes/vulnversity" | jq
-      curl -s "https://tryhackme.com/api/hacktivities" | jq
-      curl -s "https://tryhackme.com/api/new-rooms" | jq
-      curl -s "https://tryhackme.com/api/room/network/throwback" | jq
-      curl -s "https://tryhackme.com/api/room/vulnversity" | jq
       curl -s "https://tryhackme.com/api/server-time" | jq
-      curl -s "https://tryhackme.com/api/tasks/vulnversity" | jq
+
       curl -s "https://tryhackme.com/api/usersRank/7h3rAm" | jq
+      curl -s "https://tryhackme.com/api/all-completed-rooms/7h3rAm" | jq
+
+      curl -s "https://tryhackme.com/api/get-badges" | jq
+      curl -s "https://tryhackme.com/api/new-rooms" | jq
+
+      curl -s "https://tryhackme.com/api/room/network/throwback" | jq
+
+      curl -s "https://tryhackme.com/api/room/vulnversity" | jq
+      curl -s "https://tryhackme.com/api/tasks/vulnversity" | jq
+      curl -s "https://tryhackme.com/api/get-votes/vulnversity" | jq
       curl -s "https://tryhackme.com/api/video/get/vulnversity" | jq
+
     """
 
   def stats_global(self):
     # curl -s "https://tryhackme.com/api/getstats" | jq
     return self._get_http(endpoint="/getstats")
 
+  def hacktivities(self):
+    # curl -s "https://tryhackme.com/api/hacktivities" | jq
+    return self._get_http(endpoint="/hacktivities")
+
 
 if __name__ == "__main__":
   thm = TryHackMe()
-  print(thm.stats_global())
+
+  pprint(thm.hacktivities())
+
 
